0d7f1a4ba59137842e62f38c9adaa3b9700d8bb2
Ilya Bukinich <60099021+ilya-bukinich@users.noreply.github.com>
2020-06-25 00:43:14 +0300

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-consoleText(['Hello, ', 'Wordld!!!'], 'text',['white']);
+consoleText(['#Hello, Wordld!!!'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
